dec7c55db4a296289600fb113e2aae729212f8fc
Luckson Musyani <118877336+Musyani-Luckson@users.noreply.github.com>
2023-07-14 00:48:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function printEveryMillisecond() {
+	const timeEle = document.getElementById('time');
+	let count = 0;
+
+	const updateDisplay = () => {
+		const currentTime = new Date();
+		const [hour, min, sec, milSec] = [
+      currentTime.getHours(),
+      currentTime.getMinutes(),
+      currentTime.getSeconds(),
+      currentTime.getMilliseconds()
+    ].map(addLeadingZero);
+
+		timeEle.innerText = `${hour}:${min}.${sec}.${milSec}`;
+		count++;
+	};
+
+	const intervalId = setInterval(updateDisplay, 1);
+}
+
+function addLeadingZero(number) {
+	return number.toString().padStart(2, '0');
+}
+printEveryMillisecond();
